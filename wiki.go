4aa5f962d189bf4fb998fd163f99f94b396b2ae7
Onowu Christopher <totiruzi@yahoo.com>
2021-03-04 00:36:53 +0300

    Add template, edit, view and save handlers

--- wiki.go
+++ wiki.go
@@ -5,6 +5,8 @@ import (
     "log"
     "net/http"
     "html/template"
+    "regexp"
+    "errors"
 )
 
 type Page struct {
@@ -26,6 +28,7 @@ func main() {
 //     fmt.Println(string(p2.Body))
 }
 
+
 func (p *Page) save() error {
     filename := p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
@@ -43,21 +46,64 @@ func loadPage (title string) (*Page, error) {
 
 // This function handle a call to an http request
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/rainbows/"):]
-    p, _ := loadPage(title)
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
 
-    t, _ := template.ParseFiles("view.html")
-    t.execute(w, p)
+    renderTemplate(w, "view", p)
 }
 
 // This function loads the page if exits or create an html construct if it dose not
-editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r,URL.Path[len("/edit/"):]
-    p, _ := loadPage(title)
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+    p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
     }
 
-    t, _ := template.ParseFiles("edit.html")
-    t.execute(w, p)
+    renderTemplate(w, "edit", p)
 }
+
+func renderTemplate(w http.ResponseWriter, temp string, p *Page) {
+    err := templates.ExecuteTemplate(w, temp + ".html", p)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError )
+    }
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title,err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    err =p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func getTitle (w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubMatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
+    }
+    return m[2], nil // This is the second subexpression
+}
+var templates = template.Must(template.ParseFiles("edit.html, "view.html"))
+
+var validPath = rexgexp..MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
