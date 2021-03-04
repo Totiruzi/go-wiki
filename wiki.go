b8435f96b1b220ad66e5dd89e549179d260b2fba
Onowu C <onowuc@localhost.localdomain>
2021-03-04 11:00:00 +0300

    Refactor to have a function that handles response request

--- wiki.go
+++ wiki.go
@@ -1,12 +1,12 @@
 package main
 import (
-    "fmt"
     "io/ioutil"
     "log"
     "net/http"
     "html/template"
     "regexp"
-    "errors"
+//     "fmt"
+//     "errors"
 )
 
 type Page struct {
@@ -17,9 +17,9 @@ type Page struct {
 
 func main() {
 // This is the call to the main function that handles http request call to view the page
-        http.HandleFunc("/view/", viewHandler)
-        http.HandleFunc("/edit/", editHandler)
-        http.HandleFunc("/save/", handleSave)
+        http.HandleFunc("/view/", makeHandler(viewHandler))
+        http.HandleFunc("/edit/", makeHandler(editHandler))
+        http.HandleFunc("/save/", makeHandler(saveHandler))
         log.Fatal(http.ListenAndServe(":8080", nil))
 //  CREATING AND READING FROM A txt file
 //     p1 := &Page{Title: "WebWithGolang", Body: []byte("Go is the current tornado rocking it's space")}
@@ -37,7 +37,6 @@ func (p *Page) save() error {
 func loadPage (title string) (*Page, error) {
     filename := title + ".txt"
     body, err := ioutil.ReadFile(filename)
-
     if err != nil {
         return nil, err
     }
@@ -45,50 +44,28 @@ func loadPage (title string) (*Page, error) {
 }
 
 // This function handle a call to an http request
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title, err := getTitle(w, r)
-    if err != nil {
-        return
-    }
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
     p, err := loadPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
         return
     }
-
     renderTemplate(w, "view", p)
 }
 
 // This function loads the page if exits or create an html construct if it dose not
-func editHandler(w http.ResponseWriter, r *http.Request) {
-    title, err := getTitle(w, r)
-    if err != nil {
-        return
-    }
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
     }
-
     renderTemplate(w, "edit", p)
 }
 
-func renderTemplate(w http.ResponseWriter, temp string, p *Page) {
-    err := templates.ExecuteTemplate(w, temp + ".html", p)
-
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError )
-    }
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title,err := getTitle(w, r)
-    if err != nil {
-        return
-    }
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
-    err =p.save()
+    err :=p.save()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -96,14 +73,26 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-func getTitle (w http.ResponseWriter, r *http.Request) (string, error) {
-    m := validPath.FindStringSubMatch(r.URL.Path)
-    if m == nil {
-        http.NotFound(w, r)
-        return "", errors.New("Invalid Page Title")
-    }
-    return m[2], nil // This is the second subexpression
-}
-var templates = template.Must(template.ParseFiles("edit.html, "view.html"))
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
-var validPath = rexgexp..MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+func renderTemplate(w http.ResponseWriter, temp string, p *Page) {
+    err := templates.ExecuteTemplate(w, temp + ".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError )
+    }
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        // Extract page title from request and call the provided handler fn
+        m := validPath.FindStringSubmatch(r.URL.Path)
+        if m == nil {
+            http.NotFound(w, r)
+            return
+        }
+        fn(w, r, m[2])
+    }
+}
